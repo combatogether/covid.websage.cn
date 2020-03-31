1a2acfbbe57c7695d251504dab9729625735360f
dongwenxiao <cs_victor@126.com>
2020-04-01 00:35:08 +0800

    update data & update style

--- config/data5.js
+++ config/data5.js
@@ -5,7 +5,7 @@ a0222	治疗方案	Therapeutic  Schedual
 a0223	自我防护	Self  Protection
 a0224	贡献者名单	Name  List of Devotors
 a0240	抗击新冠肺炎病毒	Defeat  Covid-19
-a0241	中国方案供世界参考	Chinese  therapeutic schedul for world reference
+a0241	中国方案供世界参考	Chinese guidance on COVID-19 a reference for the world
 a0242	方案整理来源：新型冠状病毒肺炎诊疗方案(试行第七版)	Schedual  Source:Diagnosis and Treatment Protocol for Novel Coronavirus Pneumonia  (Trial Version 7).
 a0243	分享抗新冠病毒方案	Share  the therapeutic schedual of defeating Covid-19
 a0244	已分享10000000次	Already  shared 10000000 times
